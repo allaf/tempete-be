69013fc597f9e323ed352303552ffde4215bad8c
Alex Svetos <alex.svetos@gmail.com>
2020-05-15 15:35:44 +0200

    cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,10 @@ import { GameController } from './game/game.controller';
 import { WsModule } from './ws/ws.module';
 import { GameService } from './game/game.service';
 import { DataService } from './data-service/data.service';
-import { TotoService } from './data-service/toto.service';
 
 @Module({
   imports: [AuthModule, UsersModule, WsModule],
   controllers: [AppController, UsersController, GameController],
-  providers: [AppService, UserService, GameService, DataService, TotoService],
-  // exports:[TotoService]
+  providers: [AppService, UserService, GameService, DataService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { PassportModule, AuthModuleOptions } from '@nestjs/passport';
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { jwtConstants } from './constants';
